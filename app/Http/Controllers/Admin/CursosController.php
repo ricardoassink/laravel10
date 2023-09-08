42f05271253cb50f13e60de0c9c79be5fec9f0a5
ricardoassink <ric.assink@gmail.com>
2023-09-08 12:23:35 -0300

    model

--- app/Http/Controllers/Admin/CursosController.php
+++ app/Http/Controllers/Admin/CursosController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Curso;
 use Illuminate\Http\Request;
 
 class CursosController extends Controller
 {
-    public function index(){
-        return view('admin/cursos/index');
+    public function index(Curso $curso){
+
+        //$cursos = Curso::all(); Já pega tudo.
+        
+        
+        // $curso = new Curso(); não precisa pois já vem pela dependência(uso do parâmetro "Curso $curso" já traz o objeto)
+        $cursos = $curso->all(); // retorna uma collection
+
+        //dd($cursos);
+
+        return view('admin/cursos/index', compact('cursos'));
     }
 }
