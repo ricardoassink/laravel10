0ed7999800fbb98689dd5f3f4f593eca0a0f63ad
ricardoassink <ric.assink@gmail.com>
2023-09-08 13:18:02 -0300

    dados na view

--- resources/views/admin/cursos/index.blade.php
+++ resources/views/admin/cursos/index.blade.php
@@ -1,1 +1,23 @@
 <h1> Gerenciamento do Cursos </h1>
+
+
+<table>
+    <thead>
+        <th>id</th>
+        <th>Nome:</th>
+        <th>Descrição: </th>
+        <th>Status: </th>
+        <th>Ação: </th>
+    </thead>
+    <tbody>
+        @foreach ($cursos as $curso)
+            <tr>
+                <td>{{$curso->id }}</td>
+                <td>{{$curso->nome }}</td>
+                <td>{{$curso->body }}</td>
+                <td>{{$curso->status }}</td>
+                <td> -> </td>
+            </tr>
+        @endforeach
+
+</table>
