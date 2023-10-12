38d51444ad689c2e0cfa26b47796b5f2f8fb2a87
ricardoassink <ric.assink@gmail.com>
2023-10-12 14:51:35 -0300

    Novo já com layout

--- resources/views/admin/cursos/novo.blade.php
+++ resources/views/admin/cursos/novo.blade.php
@@ -3,41 +3,28 @@
 @section('title', 'Cursos')
 
 @section('header')
-<section class="container px-4 mx-auto flex justify-between items-center">
+    <section class="container px-4 mx-auto flex justify-between items-center">
 
-    <div class="flex items-center gap-x-3 ml-4">
-        <h2 class="text-lg font-medium text-gray-800 dark:text-white text-center">Cadastro de Cursos</h2>
+        <div class="flex items-center gap-x-3 ml-4">
+            <h2 class="text-lg font-medium text-gray-800 dark:text-white text-center">Cadastro de Cursos</h2>
 
-   </div>
+        </div>
 
-
-
-
-    <a href="{{ route('cursos.novo') }}"
-        class="flex items-center px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800 mr-4 text-center">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-            stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-        </svg>
-
-
-        <span>
-            Novo Curso
-        </span>
-    </a>
-
-</section>
+    </section>
 @endsection
 
 @section('content')
     {{-- <x-alert></x-alert> --}}
     <x-alert />
 
-
+    <form action="{{ route('cursos.gravar') }}" method="POST">
         <div class="flex flex-col mt-6 ml-8 mr-8">
             <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
                     <div class="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
+
+
+
                         <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
                             <thead class="bg-gray-50 dark:bg-gray-800">
                                 <tr>
@@ -59,12 +46,12 @@
                                         <div class="inline-flex items-center">
                                             <div class="flex items-center">
                                                 <div>
-                                                 
-                                                    <form action="{{ route('cursos.gravar') }}" method="POST">
 
-                                                        @include('admin.cursos.partials.form')
 
-                                                    </form>
+
+                                                    @include('admin.cursos.partials.form')
+
+
 
                                                 </div>
                                             </div>
@@ -75,10 +62,20 @@
 
                             </tbody>
                         </table>
+
+
+                    </div>
+                    <div class="inline-flex items-center">
+                        <button type="button" onclick="history.back()"
+                            class="flex items-center px-5 py-2 mt-5 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800 mr-8 mb-8 p-5">Cancelar</button>
+
+                        <button type="submit"
+                            class="flex items-center px-5 py-2 mt-5 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800 mr-8 mb-8 p-5">Cadastrar</button>
                     </div>
                 </div>
             </div>
-        
+        </div>
+    </form>
 
 
 
@@ -86,4 +83,5 @@
 
 
 
-    @endsection
+
+@endsection
